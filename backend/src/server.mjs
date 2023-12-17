c522fe405a4de5d3055ce2ede14074ee306d8c67
Niklas Rose <nkrs2321@hochschule-trier.de>
2023-12-17 14:57:41 +0100

    start server script & server.mjs

---
+++ backend/src/server.mjs
@@ -0,0 +1,10 @@
+import express from 'express';
+
+const app = express();
+const port = process.env.PORT || 8080;
+
+app.use(express.static('frontend/build'));
+
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+});
