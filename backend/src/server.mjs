b59d36d33de0160f5dff43145341b3d7e04115e1
Niklas Rose <nkrs2321@hochschule-trier.de>
2023-12-18 10:52:13 +0100

    server.mjs anpassung

--- backend/src/server.mjs
+++ backend/src/server.mjs
@@ -1,10 +1,10 @@
 import express from 'express';
 
 const app = express();
-const port = process.env.PORT || 8080;
+const port = process.argv[2] || 8080;
 
 app.use(express.static('frontend/build'));
 
 app.listen(port, () => {
-    console.log(`Server is running on http://localhost:${port}`);
+  console.log(`Server is running on http://localhost:${port}`);
 });
