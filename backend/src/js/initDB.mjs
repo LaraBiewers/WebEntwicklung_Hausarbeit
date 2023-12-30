c0b5c5ef826efeb15a7c85b9b6243d6322238579
Niklas Rose <nkrs2321@hochschule-trier.de>
2023-12-30 21:16:38 +0100

    final for PV

--- backend/src/js/initDB.mjs
+++ backend/src/js/initDB.mjs
@@ -4,8 +4,7 @@ import { MongoClient } from 'mongodb';
 import syllabificate from 'syllabificate';
 import { error } from 'console';
 
-const names_csv = 'backend/src/assets/Gesamt_Vornamen_Koeln_2010_2022_cleaned.csv';
-
+const namesCsv = 'backend/src/assets/Gesamt_Vornamen_Koeln_2010_2022_cleaned.csv';
 
 (async function () {
   let client = null;
@@ -13,30 +12,29 @@ const names_csv = 'backend/src/assets/Gesamt_Vornamen_Koeln_2010_2022_cleaned.cs
   try {
     client = new MongoClient('mongodb://127.0.0.1:27017/');
     await client.connect();
-    console.log("connection etablished");
-  }
-  catch (error) {
+    console.log('connection etablished');
+  } catch (error) {
     console.log(`Failed to connect to MongoDB: ${error}`);
     process.exit(-1);
   }
 
   try {
-    await fillData(client, names_csv);
+    await fillData(client, namesCsv);
   } catch (error) {
     console.log(error);
   }
 })();
 
-async function fillData(client, csv_path) {
-  const db = client.db("nameDB");
+async function fillData (client, csvPath) {
+  const db = client.db('nameDB');
   try {
     const collections = await db.listCollections().toArray();
-    const namesCollectionExists = collections.some(col => col.name === "names");
+    const namesCollectionExists = collections.some(col => col.name === 'names');
     if (namesCollectionExists) {
-      if (await db.collection("names").drop()){
-        console.log("database cleaned")
+      if (await db.collection('names').drop()) {
+        console.log('database cleaned');
       } else {
-        throw error("Something went wrong with the cleaning of the database");
+        throw error('Something went wrong with the cleaning of the database');
       }
     }
   } catch (error) {
@@ -44,16 +42,16 @@ async function fillData(client, csv_path) {
     client.close();
     process.exit(-1);
   }
-  const collection = db.collection("names");
+  const collection = db.collection('names');
   const promises = [];
 
-  console.log("seeding");
+  console.log('seeding');
   return new Promise((resolve, reject) => {
-    fs.createReadStream(csv_path)
+    fs.createReadStream(csvPath)
       .pipe(csv({ separator: ';' }))
       .on('data', (row) => {
-        let syllables = syllabificate.countSyllables(row['vorname']);
-        let promise = collection.insertOne({ name: row['vorname'], geschlecht: row['geschlecht'], silben: syllables });
+        const syllables = syllabificate.countSyllables(row.vorname);
+        const promise = collection.insertOne({ name: row.vorname, geschlecht: row.geschlecht, silben: syllables });
         promises.push(promise);
       })
       .on('end', async () => {
@@ -64,76 +62,8 @@ async function fillData(client, csv_path) {
           reject(error);
         }
         client.close();
+        console.log('database initialized');
       })
       .on('error', reject);
   });
 }
-
-
-/*
-// Lese die CSV-Datei ein
-
-let rows = 0;
-
-try {
-  const fileContent = fs.readFileSync('backend/src/assets/Gesamt_Vornamen_Koeln_2010_2022_cleaned.csv', 'utf8');
-  rows = fileContent.split('\n').map(row => row.split(';'));
-} catch (error) {
-  console.error(error);
-}
-
-(async function () {
-  // Verbindung zur MongoDB
-
-  let client = null;
-
-  try {
-    client = new MongoClient('mongodb://127.0.0.1:27017/');
-    await client.connect();
-  } catch (error) {
-    console.error(error);
-    process.exit(-1);
-  }
-
-  // Befülle Datenbank
-
-  try {
-    const dbName = 'dbCollection';
-    const db = client.db(dbName);
-
-    // Wenn collection bereits vorhanden, nicht neu erstellen!
-
-    const collectionName = 'alleNamen';
-    const collections = await db.listCollections().toArray();
-    const collectionExists = collections.some(coll => coll.name === collectionName);
-
-    if (collectionExists) {
-      console.log(`Die Collection "${collectionName}" in der Datenbank "${dbName}" existiert bereits.`);
-    } else {
-      const collection = db.collection(collectionName);
-      for (const [vorname, geschlecht] of rows) {
-        const anzahlSilben = syllabificate.countSyllables(vorname);
-        await collection.insertOne({ vorname, geschlecht, anzahlSilben });
-      }
-      console.log('Datenbank erfolgreich befüllt!');
-    }
-
-    // Inhalt der Collection "alleNamen" als JSON-File abspeichern.
-
-    const collection = db.collection(collectionName);
-    const collectionContent = await collection.find({}).toArray();
-    const outputFilePath = 'backend/src/assets/Collection_alleNamen.json';
-
-    try {
-      fs.writeFileSync(outputFilePath, JSON.stringify(collectionContent, null, 2));
-      console.log(`Der Inhalt der Collection "${collectionName}" wurde als JSON in die Datei "${outputFilePath}" geschrieben.`);
-    } catch {
-      console.log('Fehler beim erstellen der Collection als .JSON-File.');
-    }
-  } catch (err) {
-    console.error('Fehler beim Befüllen der Datenbank:', err);
-  } finally {
-    await client.close();
-  }
-})();
-*/
--- backend/src/js/server.mjs
+++ backend/src/js/server.mjs
@@ -3,7 +3,7 @@ import express from 'express';
 const app = express();
 const port = process.argv[2] || 8080;
 
-app.use(express.static('frontend/build'));
+app.use(express.static('build'));
 
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
