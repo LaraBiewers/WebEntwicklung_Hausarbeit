cc4882259cb1002d151737c2f04bb6069e30732e
Niklas Rose <nkrs2321@hochschule-trier.de>
2024-01-04 00:05:13 +0100

    router "/names"

--- backend/src/js/server.mjs
+++ backend/src/js/server.mjs
@@ -1,27 +1,19 @@
 import express from 'express';
-import { MongoClient } from 'mongodb';
-import "dotenv/config.js";
+import namesRouter from '../routes/names.mjs';
 
 const app = express();
 const port = process.argv[2] || 8080;
 
 app.use(express.static('build'));
 
-let db;
-
-MongoClient.connect(process.env.DATABASE_URL)
-   .then((client) => {
-       console.log('Connected to Database');
-       db = client.db(); 
-   })
-   .catch((err) => console.error(err));
-console.log(db);
-
+app.use('/names', namesRouter);
+/*
 app.get('/api/names', async (req, res) => {
   const skip = parseInt(req.query.skip) || 0;
   const namesData = await db.collection('names').find().skip(skip).limit(10).toArray();
   res.json(namesData);
 });
+*/
 
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
---
+++ backend/src/routes/names.mjs
@@ -0,0 +1,27 @@
+import express from 'express';
+import { MongoClient } from 'mongodb';
+import 'dotenv/config.js';
+
+const router = express.Router();
+
+let db;
+
+MongoClient.connect(process.env.DATABASE_URL)
+    .then((client) => {
+        db = client.db();
+    })
+    .catch((err) => console.error(err));
+
+// Getting all
+router.get('/', async (req, res) => {
+    try {
+        const skip = parseInt(req.query.skip) || 0;
+        const limit = parseInt(req.query.limit) || 10;
+        const namesData = await db.collection('names').find().skip(skip).limit(limit).toArray();
+        res.json(namesData);
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+});
+
+export default router;
