ec07b48ff9abe51b555c8f7cdac7b01371894451
LaraBiewers <larabiewers@gmail.com>
2024-01-12 13:27:05 +0100

    Grundstyle Webseite

--- backend/src/routes/names.mjs
+++ backend/src/routes/names.mjs
@@ -7,21 +7,21 @@ const router = express.Router();
 let db;
 
 MongoClient.connect(process.env.DATABASE_URL)
-    .then((client) => {
-        db = client.db();
-    })
-    .catch((err) => console.error(err));
+  .then((client) => {
+    db = client.db();
+  })
+  .catch((err) => console.error(err));
 
 // Getting all
 router.get('/', async (req, res) => {
-    try {
-        const skip = parseInt(req.query.skip) || 0;
-        const limit = parseInt(req.query.limit) || 10;
-        const namesData = await db.collection('names').find().skip(skip).limit(limit).toArray();
-        res.json(namesData);
-    } catch (error) {
-        res.status(500).json({ message: error.message });
-    }
+  try {
+    const skip = parseInt(req.query.skip) || 0;
+    const limit = parseInt(req.query.limit) || 10;
+    const namesData = await db.collection('names').find().skip(skip).limit(limit).toArray();
+    res.json(namesData);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
 });
 
 export default router;
