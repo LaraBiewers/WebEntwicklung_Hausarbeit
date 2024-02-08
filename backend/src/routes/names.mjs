5c4cb2bf1a633c2651d9a30ffa0017e799ad6267
Niklas Rose <nkrs2321@hochschule-trier.de>
2024-02-08 19:43:03 +0100

    dead code removed

--- backend/src/routes/names.mjs
+++ backend/src/routes/names.mjs
@@ -44,32 +44,4 @@ router.get('/', async (req, res) => {
   }
 });
 
-router.get('/count', async (req, res) => {
-  try {
-    const sex = req.query.sex;
-    const prefix = req.query.prefix;
-    const notPrefix = req.query.notPrefix;
-    const suffix = req.query.suffix;
-    const notSuffix = req.query.notSuffix;
-    const syllables = parseInt(req.query.syllables);
-
-    const query = {};
-    if (sex) query.geschlecht = sex;
-    if (prefix) query.$and = query.$and || [];
-    if (prefix) query.$and.push({ name: { $regex: `^${prefix}` } });
-    if (notPrefix) query.$and = query.$and || [];
-    if (notPrefix) query.$and.push({ name: { $not: { $regex: `^${notPrefix}` } } });
-    if (suffix) query.$and = query.$and || [];
-    if (suffix) query.$and.push({ name: { $regex: `${suffix}$` } });
-    if (notSuffix) query.$and = query.$and || [];
-    if (notSuffix) query.$and.push({ name: { $not: { $regex: `${notSuffix}$` } } });
-    if (syllables) query.silben = { $eq: syllables };
-
-    const count = await db.collection('names').countDocuments(query);
-    res.json({ count });
-  } catch (error) {
-    res.status(500).json({ message: error.message });
-  }
-});
-
 export default router;
