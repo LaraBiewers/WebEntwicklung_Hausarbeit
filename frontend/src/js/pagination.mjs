25e73cc5af177f5850274b3734b962e2e981f892
LaraBiewers <larabiewers@gmail.com>
2024-02-21 10:40:00 +0100

    delete Playground, requests converted to RESTfull

--- frontend/src/js/pagination.mjs
+++ frontend/src/js/pagination.mjs
@@ -1,37 +1,63 @@
 const pageSize = 10;
 let currentPage = 1;
 let totalPages = 0;
+// Flag to track if the watchlist is active
+let isWatchlistActive = false;
+
 document.addEventListener('DOMContentLoaded', (event) => {
-  fetchNames(currentPage);
+  resetPageAndFetch();
 });
+
+function resetPageAndFetch () {
+  currentPage = 1;
+  fetchAndUpdate();
+}
+
+function fetchAndUpdate () {
+  if (isWatchlistActive) {
+    fetchNamesFromWatchlist(currentPage);
+  } else {
+    fetchNamesFromNames(currentPage);
+  }
+}
+
+function toggleWatchlistAndReset () {
+  isWatchlistActive = !isWatchlistActive; // Toggle the flag
+  resetPageAndFetch(); // Reset the page and fetch the appropriate data
+}
 
 const nextButton = document.getElementById('next');
 const prevButton = document.getElementById('previous');
 const firstButton = document.getElementById('first');
 const lastButton = document.getElementById('last');
+const fullListButton = document.getElementById('Namensliste');
+const watchListButton = document.getElementById('Merkliste');
+
 nextButton.addEventListener('click', nextPage);
 prevButton.addEventListener('click', previousPage);
 firstButton.addEventListener('click', firstPage);
 lastButton.addEventListener('click', lastPage);
+fullListButton.addEventListener('click', toggleWatchlistAndReset);
+watchListButton.addEventListener('click', toggleWatchlistAndReset);
 
 function nextPage () {
   currentPage++;
-  fetchNames(currentPage);
+  fetchAndUpdate();
 }
 
 function previousPage () {
   currentPage--;
-  fetchNames(currentPage);
+  fetchAndUpdate();
 }
 
 function firstPage () {
   currentPage = 1;
-  fetchNames(currentPage);
+  fetchAndUpdate();
 }
 
 function lastPage () {
   currentPage = totalPages;
-  fetchNames(currentPage);
+  fetchAndUpdate();
 }
 
 function checkPageForButtonDisable () {
@@ -52,28 +78,11 @@ function checkPageForButtonDisable () {
   }
 }
 
-// Fill <ol>-Elements
-async function fetchNames (currentPage) {
-  // If "complete name list" active, use all filters, otherwise only gender
-  const sex = document.getElementById('sex').value;
-  const prefix = document.getElementById('prefix').value;
-  const notPrefix = document.getElementById('notPrefix').value;
-  const suffix = document.getElementById('suffix').value;
-  const notSuffix = document.getElementById('notSuffix').value;
-  const syllables = document.getElementById('syllables').value;
+/// //
+//  Communication with Backend
+/// //
 
-  // Fetch the filtered data from names-Collection
-  const skipValue = (currentPage - 1) * pageSize;
-  const response = await fetch(`/names?skip=${skipValue}&limit=${pageSize}&sex=${sex}&prefix=${prefix}&notPrefix=${notPrefix}&suffix=${suffix}&notSuffix=${notSuffix}&syllables=${syllables}`);
-  const names = await response.json();
-
-  totalPages = Math.ceil(names.count / pageSize);
-  // Update names table with filtered data
-  updateNamesTable(names.namesData);
-  checkPageForButtonDisable();
-}
-
-// Buttonfunktion: Kopiere Element über _ID von names nach watchlist
+// Buttonfunktion: Kopiere Element von names nach watchlist
 async function addToWatchlist (id) {
   const response = await fetch('/addToWatchlist', {
     method: 'POST',
@@ -83,8 +92,83 @@ async function addToWatchlist (id) {
     body: JSON.stringify({ id })
   });
 
+  if (!response.ok) {
+    throw new Error(`HTTP error! status: ${response.status}`);
+  }
+
   const data = await response.json();
   console.log(data);
+}
+
+// Buttonfunktion: Priorisiere Element von watchlist
+async function prioriseElement (id) {
+  const response = await fetch('/priorisingWatchlistElements', {
+    method: 'PATCH',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({ id })
+  });
+
+  if (!response.ok) {
+    throw new Error(`HTTP error! status: ${response.status}`);
+  }
+
+  const data = await response.json();
+  console.log(data);
+}
+
+// Buttonfunktion: Lösche Element von watchlist
+async function deleteFromWatchlist (id) {
+  const response = await fetch('/deleteFromWatchlist', {
+    method: 'DELETE',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({ id })
+  });
+
+  if (!response.ok) {
+    throw new Error(`HTTP error! status: ${response.status}`);
+  }
+
+  const data = await response.json();
+  console.log(data);
+}
+
+/// //
+//  Code for names-table
+/// //
+
+// Fill names-table
+async function fetchNamesFromNames (currentPage) {
+  // Filtereingaben der names-table
+  const sex = document.getElementById('sex').value;
+  const prefix = document.getElementById('prefix').value;
+  const notPrefix = document.getElementById('notPrefix').value;
+  const suffix = document.getElementById('suffix').value;
+  const notSuffix = document.getElementById('notSuffix').value;
+  const syllables = document.getElementById('syllables').value;
+
+  // Fetch filtered data from names-Collection
+  const skipValue = (currentPage - 1) * pageSize;
+  const response = await fetch(`/names?skip=${skipValue}&limit=${pageSize}&sex=${sex}&prefix=${prefix}&notPrefix=${notPrefix}&suffix=${suffix}&notSuffix=${notSuffix}&syllables=${syllables}`, {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  });
+
+  if (!response.ok) {
+    throw new Error(`HTTP error! status: ${response.status}`);
+  }
+
+  const names = await response.json();
+
+  // Befülle Tabelle dementsprechend der Eingaben
+  totalPages = Math.ceil(names.count / pageSize);
+  updateNamesTable(names.namesData);
+  checkPageForButtonDisable();
 }
 
 // Build up table structure
@@ -93,6 +177,33 @@ async function updateNamesTable (namesData) {
 
   // Clear the table
   namesTable.innerHTML = '';
+
+  // Hinzufügen von Überschriften für die Liste
+  const headerElement = document.createElement('li');
+  headerElement.className = 'headerContainer';
+
+  const nameContainer = document.createElement('div');
+  nameContainer.className = 'nameContainer';
+  nameContainer.textContent = 'Vorname';
+
+  const sexContainer = document.createElement('div');
+  sexContainer.className = 'sexContainer';
+  sexContainer.textContent = 'Geschlecht';
+
+  const syllablesContainer = document.createElement('div');
+  syllablesContainer.className = 'syllablesContainer';
+  syllablesContainer.textContent = 'Silbenanzahl';
+
+  // button to copy elements to watchlist
+  const copyButtonContainer = document.createElement('div');
+  copyButtonContainer.className = 'buttonContainer';
+  copyButtonContainer.textContent = 'Zur Merkliste hinzufügen';
+
+  namesTable.appendChild(headerElement);
+  headerElement.appendChild(nameContainer);
+  headerElement.appendChild(sexContainer);
+  headerElement.appendChild(syllablesContainer);
+  headerElement.appendChild(copyButtonContainer);
 
   namesData.forEach(element => {
     const dataContainer = document.createElement('li');
@@ -127,15 +238,170 @@ async function updateNamesTable (namesData) {
       addToWatchlist(id);
     });
 
+    namesTable.appendChild(dataContainer);
+
     dataContainer.appendChild(nameContainer);
     dataContainer.appendChild(sexContainer);
     dataContainer.appendChild(syllablesContainer);
     dataContainer.appendChild(copyButtonContainer);
     copyButtonContainer.appendChild(copyButton);
-
-    namesTable.appendChild(dataContainer);
   });
 }
+
+/// //
+//  Code for watchlist-table
+/// //
+
+// Fill watchlist-table
+async function fetchNamesFromWatchlist (currentPage) {
+  // Filtereingaben der watchlist-table
+  const sex = document.getElementById('sex').value;
+  const prio = document.getElementById('prio').value;
+
+  // Fetch filtered data from watchlist-Collection
+  const skipValue = (currentPage - 1) * pageSize;
+  const response = await fetch(`/getFromWatchlist?skip=${skipValue}&limit=${pageSize}&sex=${sex}&prio=${prio}`, {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  });
+
+  if (!response.ok) {
+    throw new Error(`HTTP error! status: ${response.status}`);
+  }
+
+  const names = await response.json();
+
+  // Befülle Tabelle dementsprechend der Eingaben
+  totalPages = Math.ceil(names.count / pageSize);
+  updateWatchlistTable(names.watchlistData);
+  checkPageForButtonDisable();
+}
+
+// Build up table structure for watchlist-table
+async function updateWatchlistTable (watchlistData) {
+  const watchlistTable = document.getElementById('watchlist-table');
+
+  // Clear the table
+  watchlistTable.innerHTML = '';
+
+  // Hinzufügen von Überschriften für die Liste
+  const headerElement = document.createElement('li');
+  headerElement.className = 'headerContainer';
+
+  const nameContainer = document.createElement('div');
+  nameContainer.className = 'nameContainer';
+  nameContainer.textContent = 'Vorname';
+
+  const sexContainer = document.createElement('div');
+  sexContainer.className = 'sexContainer';
+  sexContainer.textContent = 'Geschlecht';
+
+  const syllablesContainer = document.createElement('div');
+  syllablesContainer.className = 'syllablesContainer';
+  syllablesContainer.textContent = 'Silbenanzahl';
+
+  const prioParaContainer = document.createElement('div');
+  prioParaContainer.className = 'prioParaContainer';
+  prioParaContainer.innerText = 'Prioritätsstatus';
+
+  // Button zum anwählen der Priorisierung
+  const prioButtonContainer = document.createElement('div');
+  prioButtonContainer.className = 'prioButtonContainer';
+  prioButtonContainer.textContent = 'Vorname priorisieren';
+
+  // button to delete elements from watchlist
+  const deleteButtonContainer = document.createElement('div');
+  deleteButtonContainer.className = 'buttonContainer';
+  deleteButtonContainer.textContent = 'Vorname entfernen';
+
+  watchlistTable.appendChild(headerElement);
+  headerElement.appendChild(nameContainer);
+  headerElement.appendChild(sexContainer);
+  headerElement.appendChild(syllablesContainer);
+  headerElement.appendChild(prioParaContainer);
+  headerElement.appendChild(prioButtonContainer);
+  headerElement.appendChild(deleteButtonContainer);
+
+  watchlistData.forEach(element => {
+    const dataContainer = document.createElement('li');
+    dataContainer.className = 'dataContainer';
+    dataContainer.id = element._id;
+
+    const nameContainer = document.createElement('div');
+    nameContainer.className = 'nameContainer';
+    nameContainer.textContent = element.name;
+
+    const sexContainer = document.createElement('div');
+    sexContainer.className = 'sexContainer';
+    sexContainer.textContent = element.geschlecht;
+
+    const syllablesContainer = document.createElement('div');
+    syllablesContainer.className = 'syllablesContainer';
+    syllablesContainer.textContent = element.silben;
+
+    // Exklusiv für watchlist
+
+    // Anzeige des Prio-Status
+    const prioParaContainer = document.createElement('div');
+    prioParaContainer.className = 'prioParaContainer';
+    prioParaContainer.innerText = element.prio;
+
+    // Button zum anwählen der Priorisierung
+    const prioButtonContainer = document.createElement('div');
+    prioButtonContainer.className = 'prioButtonContainer';
+
+    const prioButton = document.createElement('button');
+    prioButton.setAttribute('type', 'button');
+    prioButton.className = 'prioButton';
+    prioButton.textContent = 'Priorisieren';
+
+    // button to delete elements from watchlist
+    const deleteButtonContainer = document.createElement('div');
+    deleteButtonContainer.className = 'buttonContainer';
+
+    const deleteButton = document.createElement('button');
+    deleteButton.setAttribute('type', 'button');
+    deleteButton.className = 'deleteFromWatchlist';
+    deleteButton.textContent = 'Vorname entfernen';
+
+    // EventListener für Priorisierung
+    prioButton.addEventListener('click', () => {
+      // zugehörige ID des <li> Elements
+      const id = dataContainer.id;
+      prioriseElement(id);
+      // Nach dem Löschen aktualisiere die watchlist-Tabelle
+      fetchNamesFromWatchlist(currentPage);
+    });
+
+    // EventListener für addToWatchlist
+    deleteButton.addEventListener('click', () => {
+      // zugehörige ID des <li> Elements
+      const id = dataContainer.id;
+      deleteFromWatchlist(id);
+      // Nach dem Löschen aktualisiere die watchlist-Tabelle
+      fetchNamesFromWatchlist(currentPage);
+    });
+
+    watchlistTable.appendChild(dataContainer);
+
+    dataContainer.appendChild(nameContainer);
+    dataContainer.appendChild(sexContainer);
+    dataContainer.appendChild(syllablesContainer);
+    dataContainer.appendChild(prioParaContainer);
+
+    dataContainer.appendChild(prioButtonContainer);
+    prioButtonContainer.appendChild(prioButton);
+
+    dataContainer.appendChild(deleteButtonContainer);
+    deleteButtonContainer.appendChild(deleteButton);
+  });
+}
+
+/// //
+//  Export-functions
+/// //
 
 export function getCurrentPage () {
   return currentPage;
@@ -148,5 +414,10 @@ export function setCurrentPage (newValue) {
     console.log('Page out of range, set to first page');
     currentPage = 1;
   }
-  fetchNames(currentPage);
+  // Call the appropriate fetch function based on whether the watchlist is active
+  if (isWatchlistActive) {
+    fetchNamesFromWatchlist(currentPage);
+  } else {
+    fetchNamesFromNames(currentPage);
+  }
 }
